8baa532caf84d36953bd8a2817d6bade3f7c9b91
kishansinha181 <54812321+kishansinha181@users.noreply.github.com>
2020-12-20 13:20:01 +0530

    Update app_startup_code.js

--- jscript/app_startup_code.js
+++ jscript/app_startup_code.js
@@ -33,8 +33,8 @@ function predictOnLoad() {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('http://dr.test.woza.work/model_dr_2/model.json');
-	$("#selected-image").attr("src", "http://dr.test.woza.work/assets/fundus_1.jpg");
+	model = await tf.loadModel('https://dr.test.woza.work/model_dr_2/model.json');
+	$("#selected-image").attr("src", "https://dr.test.woza.work/assets/fundus_1.jpg");
 	
 	// Hide the model loading spinner
 	// This line of html gets hidden:
